cf6e13d466545b656ee9edb18ad845ac8dfb2e65
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-02-17 15:44:19 -0500

    added tests for menu class

--- spec/nearby_restaurants_spec.rb
+++ spec/nearby_restaurants_spec.rb
@@ -2,10 +2,27 @@ require_relative 'spec_helper'
 
 describe NearbyRestaurants do
 	
-	rest = NearbyRestaurants.new("11 Broadway 10004")
+	rest_spaces = NearbyRestaurants.new("11 Broadway 10004")
+	rest_commas = NearbyRestaurants.new("11 Broadway, New York, NY 10004")
+
+	it 'accepts an address with spaces'  do
+		expect(rest_spaces.restaurants.count).to be >(0)
+	end
+
+	it 'accepts an address with commas' do
+		expect(rest_commas.restaurants.count).to be >(0)
+	end
 
 	it 'returns a list of restaurants nearby' do
-		expect(NearbyRestaurants.list.count).to be >(0)
+		expect(rest_commas.restaurants.count).to be >(0)
+	end
+
+	it 'returns a list of merchant ids' do
+		expect(rest_commas.ids.count).to be >(0)
+	end
+
+	it 'returns a random merchant id' do
+		expect(rest_commas.random_merchant.to_i).to be >(0)
 	end
 
 end

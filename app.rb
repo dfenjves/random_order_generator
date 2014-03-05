d8191944c45c3249484560ea5cb342bf05d815d5
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-03-05 16:57:11 -0500

    sessions added, retry button added

--- app.rb
+++ app.rb
@@ -2,25 +2,31 @@ class ROGApp < Sinatra::Base
  configure :development do
     register Sinatra::Reloader
  end
+ enable :sessions
 
-get '/' do
-	erb :main
-end
+	get '/' do
+		erb :main
+	end
 
-post '/' do
-	address = params[:address]
-	quantity = params[:items].to_i
-	max = params[:maxprice].to_i
+	post '/' do
+		session[:address] = params[:address]
+		session[:quantity] = params[:items].to_i
+		session[:max] = params[:maxprice].to_i
 
-	begin
-		@order = RandomOrderGenerator.new(address,quantity,max)
-	rescue
-		erb :badaddress
-	else
+		begin
+			@order = RandomOrderGenerator.new(session[:address],session[:quantity],session[:max])
+		rescue
+			erb :badaddress
+		else
+			erb :result
+		end
+		
+	end
+
+	get '/moar' do
+		@order = RandomOrderGenerator.new(session[:address],session[:quantity],session[:max])
 		erb :result
 	end
-	
-end
 
 
 
